d51382c31d3130d56ccd9a95d1bd8ff70c4be2bb
kmh0831 <kmhyuk1018@gmail.com>
2024-09-06 19:51:15 +0900

    v1

--- front/public/scripts.js
+++ front/public/scripts.js
@@ -1,13 +1,16 @@
 let slideIndex = 0;
 const slides = document.querySelectorAll(".slides");
 
+// 첫 번째 슬라이드 활성화
+slides[0].classList.add("active");
+
 function showSlides() {
     for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove("active");
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].classList.add("active");
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    slides[slideIndex].classList.add("active");
     setTimeout(showSlides, 3000); // 3초마다 슬라이드 전환
 }
 
